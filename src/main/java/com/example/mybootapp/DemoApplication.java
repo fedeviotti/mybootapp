1b711de1daa9b6a243321808bbc28a506c4cd551
fedeviotti <fedeviotti@gmail.com>
2018-12-28 11:12:12 +0100

    aggiunto greeting rest

--- src/main/java/com/example/mybootapp/DemoApplication.java
+++ src/main/java/com/example/mybootapp/DemoApplication.java
@@ -2,20 +2,13 @@ package com.example.mybootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice
 @SpringBootApplication
 public class DemoApplication {
 
-	@RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
 }
 
